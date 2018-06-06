21c6c7c530a85567ce64d4b067dc1de97b55f729
Luke Prescott <lprescott@users.noreply.github.com>
2018-06-05 21:11:06 -0400

    Finalized form pages

--- php/contact-form.php
+++ php/contact-form.php
@@ -1,52 +1,53 @@
 <?php
   if($_POST['formSubmit'] == "Submit")
-  {
-    $errorMessage = "";
+    {
+        $errorMessage = "";
 
-    if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['comment'])) {
-        $errorMessage .= "<li>Required information is missing.</li>";
+        if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['comment'])) {
+            $errorMessage .= "<li>Required information is missing.</li>";
+        }
+
+        $varName = $_POST['name'];
+        $varEmail = $_POST['email'];
+        $varPhone = $_POST['phone'];
+        $varSubject = $_POST['subject'];
+        $varComment = $_POST['comment'];
+
+        $to = "info@presport.us";
+
+        if(!empty($errorMessage)) {
+            header('Location: ../form/inquiries.failure.html');  
+            exit;
+        } 
+
+        $msg = wordwrap($varComment,70);
+        $header = "From: $varEmail"; 
+
+        /* All data */
+        if(!empty($_POST['telephone']) && !empty($_POST['subject'])){
+            $msg .= "\n\n$varName\n$varEmail\n$varPhone";
+            mail($to,$varSubject,$msg,$header);
+        }
+
+        /* missing telephone */
+        if(empty($_POST['telephone']) && !empty($_POST['subject'])){
+            $msg .= "\n\n$varName\n$varEmail";
+            mail($to,$varSubject,$msg,$header);
+        }
+
+        /* missing subject */
+        if(empty($_POST['subject']) && !empty($_POST['telephone'])){
+            $msg .= "\n\n$varName\n$varEmail\n$varPhone";
+            mail($to,"PresPort Contact Form",$msg,$header);
+        }
+
+        /* missing telephone and subject */
+        if(empty($_POST['telephone']) && empty($_POST['subject'])){
+            $msg .= "\n\n$varName\n$varEmail";
+            mail($to,"PresPort Contact Form",$msg,$header);
+        }
+
+        header('Location: ../form/inquiries.success.html');  
+        exit;
     }
-
-    $varName = $_POST['name'];
-    $varEmail = $_POST['email'];
-    $varPhone = $_POST['phone'];
-    $varSubject = $_POST['subject'];
-    $varComment = $_POST['comment'];
-
-    $to = "info@presport.us";
-
-    if(!empty($errorMessage)) {
-        echo("<p>There was an error with your form:</p>\n");
-        echo("<ul>" . $errorMessage . "</ul>\n");
-    } 
-
-    $msg = wordwrap($varComment,70);
-    $header = "From: $varEmail"; 
-
-    /* All data */
-    if(!empty($_POST['telephone']) && !empty($_POST['subject'])){
-        $msg .= "\n\n$varName\n$varEmail\n$varPhone";
-        mail($to,$varSubject,$msg,$header);
-    }
-
-    /* missing telephone */
-    if(empty($_POST['telephone']) && !empty($_POST['subject'])){
-        $msg .= "\n\n$varName\n$varEmail";
-        mail($to,$varSubject,$msg,$header);
-    }
-
-    /* missing subject */
-    if(empty($_POST['subject']) && !empty($_POST['telephone'])){
-        $msg .= "\n\n$varName\n$varEmail\n$varPhone";
-        mail($to,"PresPort Contact Form",$msg,$header);
-    }
-
-    /* missing telephone and subject */
-    if(empty($_POST['telephone']) && empty($_POST['subject'])){
-        $msg .= "\n\n$varName\n$varEmail";
-        mail($to,"PresPort Contact Form",$msg,$header);
-    }
-
-    header('Location: ../form/thankyou.html');  
-}
 ?>
