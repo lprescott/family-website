51b76c478e7dbf27b9cac51875d4d753682f5623
Luke Prescott <lukep@nycap.rr.com>
2019-01-07 16:23:01 -0500

    Initial Commit for Compatibility Rework

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php //begin php
+
+?>
+<!-- end php -->
+
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+
+  <meta name="robots" content="index, follow">
+  <meta name="keywords" content="prescott, presport, presport.us, information technology, program management consulting services, creative writing, secondary education in English language arts, medicine, nursing, computer science, mathematics">
+
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>PresPort</title>
+  <meta name="description" content="Presport is the Prescott family hub – geographically located the capital district of New York State. The Prescott family has a total of 20 years (and counting) in upper-level education, leading to expertise in a plethora of areas; they include: information technology, program management consulting services, creative writing, secondary education in English language arts, medicine, nursing,computer science, and mathematics.">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-touch-icon.png">
+  <link rel="icon" type="image/x-icon" sizes="32x32" href="/img/icons/favicon-32x32.png">
+  <link rel="icon" type="image/x-icon" sizes="16x16" href="/img/icons/favicon-16x16.png">
+  <link rel="manifest" href="/img/icons/site.webmanifest">
+  <link rel="mask-icon" href="/img/icons/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="shortcut icon" href="/img/icons/favicon.ico" type="image/x-icon">
+  <meta name="msapplication-TileColor" content="#2b5797">
+  <meta name="msapplication-config" content="/img/icons/browserconfig.xml">
+  <meta name="theme-color" content="#ffffff">
+
+  <link rel="manifest" href="site.webmanifest">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+    crossorigin="anonymous">
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+  <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <!-- Start site/application content here -->
+  <div id="main">
+    <header class="rounded-corners padded shadow-hover margin-all">
+      Hello World!
+    </header>
+
+    <div id="spacer"></div>
+
+    <footer class="rounded-corners padded shadow-hover margin-left margin-right">
+      Powered by
+      <a class="link-style" href="https://www.javascript.com/">javascript</a> and started with
+      <a class="link-style" href="https://html5boilerplate.com/" target="_blank" rel="noopener">html5boilerplate</a>.
+      <br> General questions, comments, or concerns?
+      <a class="link-style" href="http://presport.us/inquiries/contact.php"> Contact us!</a>
+      This website was designed and built by Luke Prescott.
+      <br> Any and all trademarks or logos used throughout this website are the property of their respective owners.
+    </footer>
+  </div> <!-- end main -->
+
+  <!-- End site/application content here -->
+  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+  <script>
+    window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')
+
+  </script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. (111332341-1) -->
+  <script>
+    window.ga = function () {
+      ga.q.push(arguments)
+    };
+    ga.q = [];
+    ga.l = +new Date;
+    ga('create', 'UA-111332341-1', 'auto');
+    ga('send', 'pageview')
+
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+
+</html>
