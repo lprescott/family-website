f96281ef6b9bc8a2162b3e754be09483743c5e47
Luke Prescott <lukep@nycap.rr.com>
2019-01-09 17:59:08 -0500

    Completion of navbar

--- index.php
+++ index.php
@@ -35,6 +35,7 @@
 
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/main.css">
+
 </head>
 
 <body>
@@ -46,74 +47,136 @@
   <div id="main">
     <header class="rounded-corners shadow-hover margin-all">
       <nav class="nav">
-        <ul>
-          <!--
-          <li>
-            <a title="Home" href="index.html">
-              <img alt="Home" src="/img/Home.png" height="40">
+        <ul class="rounded-corners">
+
+          <li class="home-button">
+            <a style="padding: 9px 11px 9px 11px; " title="Home" href="index.php">
+              <img alt="Home" src="/img/Home.png" height="40px">
             </a>
           </li>
-          -->
+
           <!-- people dropdown -->
-          <li class="dropdown">
+          <li class="dropdown desktop-button">
             <a>People
-              <span class="caret"></span>
+              <i style="font-size: 12px;" class="fas fa-angle-down"></i>
             </a>
-            
-              <ul>
-                <li>
-                  <a href="/people/drewprescottsr.html">Drew Prescott, Sr.</a>
-                </li>
-                <li>
-                  <a href="/people/karenprescott.html">Karen Prescott</a>
-                </li>
-                <li>
-                  <a href="/people/barbaraprescott.html">Barbara Prescott</a>
-                </li>
-                <li class="bottom">
-                  <a href="/people/lprescott.html">Luke Prescott</a>
-                </li>
-              </ul>
-            
+
+            <ul>
+              <li class="top-dropdown-item" style="width: 200px;">
+                <a href="/people/drewprescottsr.html">Drew Prescott, Sr.</a>
+              </li>
+              <li style="width: 200px;">
+                <a href="/people/karenprescott.html">Karen Prescott</a>
+              </li>
+              <li style="width: 200px;">
+                <a href="/people/barbaraprescott.html">Barbara Prescott</a>
+              </li>
+              <li class="bottom-dropdown-item" style="width: 200px;">
+                <a href="/people/lprescott.html">Luke Prescott</a>
+              </li>
+            </ul>
+
           </li>
 
           <!-- property dropdown-->
-          <li class="dropdown">
+          <li class="dropdown desktop-button">
             <a>Property
-              <span class="caret"></span>
+              <i style="font-size: 12px;" class="fas fa-angle-down"></i>
             </a>
-            
-              <ul>
-                <li>
-                  <a href="/property/inside.html">Inside</a>
-                </li>
-                <li>
-                  <a href="/property/outside.html">Outside</a>
-                </li>
-                <li class="bottom">
-                  <a href="/property/location.html">Location</a>
-                </li>
-              </ul>
-            
+
+            <ul>
+              <li class="top-dropdown-item" style="width: 123.77px;">
+                <a href="/property/inside.html">Inside</a>
+              </li>
+              <li style="width: 123.77px;">
+                <a href="/property/outside.html">Outside</a>
+              </li>
+              <li class="bottom-dropdown-item" style="width: 123.77px;">
+                <a href="/property/location.html">Location</a>
+              </li>
+            </ul>
+
           </li>
 
           <!-- projects dropdown-->
-          <li class="dropdown">
+          <li class="dropdown desktop-button">
             <a>Projects
-              <span class="caret"></span>
+              <i style="font-size: 12px;" class="fas fa-angle-down"></i>
             </a>
-           
-              <ul>
-                <li>
-                  <a href="../projects/OAP.html">Ocular Audiobook Player</a>
-                </li>
-                <li>
-                  <a href="../projects/pre-Sportus.html">pre-Sport us!</a>
-                </li>
-              </ul>
-            
+
+            <ul>
+              <li class="top-dropdown-item" style="width: 270px;">
+                <a href="../projects/OAP.html">Ocular Audiobook Player</a>
+              </li>
+              <li class="bottom-dropdown-item" style="width: 270px;">
+                <a href="../projects/pre-Sportus.html">pre-Sport us!</a>
+              </li>
+            </ul>
+
           </li>
-          
+
+          <li class="contact-button">
+            <a style="padding: 13px 15px 14px 15px;" title="Contact Us" href="inquiries/contact.php">
+              <span style="font-size: 25px; color: rgb(75,75,75);">
+                <i class="fas fa-mail-bulk"></i>
+              </span>
+            </a>
+          </li>
+
+          <li class="dropdown mobile-button">
+            <a style="padding: 12.5px 15px 12.5px 15px;" title="Menu">
+              <span style="font-size: 30px; color: rgb(75,75,75);">
+                <i class="fas fa-bars"></i>
+              </span>
+            </a>
+
+            <ul>
+              <li id="people" class="top-dropdown-item" style="width: 270px;">
+                <a href="#" onclick="showOrHide('person', 'projects');">People
+                  <i style="font-size: 12px;" class="fas fa-angle-down"></i>
+                </a>
+              </li>
+              <li class="sub-item person" style="width: 270px;">
+                <a href="/people/drewprescottsr.html">Drew Prescott, Sr.</a>
+              </li>
+              <li class="sub-item person" style="width: 270px;">
+                <a href="/people/karenprescott.html">Karen Prescott</a>
+              </li>
+              <li class="sub-item person" style="width: 270px;">
+                <a href="/people/barbaraprescott.html">Barbara Prescott</a>
+              </li>
+              <li class="sub-item person" style="width: 270px;">
+                <a href="/people/lprescott.html">Luke Prescott</a>
+              </li>
+              <li id="property" style="width: 270px;">
+                <a href="#" onclick="showOrHide('property', 'projects');">Property
+                  <i style="font-size: 12px;" class="fas fa-angle-down"></i>
+                </a>
+              </li>
+              <li class="sub-item property" style="width: 270px;">
+                <a href="/property/inside.html">Inside</a>
+              </li>
+              <li class="sub-item property" style="width: 270px;">
+                <a href="/property/outside.html">Outside</a>
+              </li>
+              <li class="sub-item property" style="width: 270px;">
+                <a href="/property/location.html">Location</a>
+              </li>
+              <li id="projects" class="bottom-dropdown-item" style="width: 270px;">
+               <a href="#" onclick="showOrHide('project', 'projects');">Projects
+                  <i style="font-size: 12px;" class="fas fa-angle-down"></i>
+                </a>
+              </li>
+              <li class="sub-item project" style="width: 270px;">
+                <a href="../projects/OAP.html">Ocular Audiobook Player</a>
+              </li>
+              <li class="sub-item bottom-dropdown-item project" style="width: 270px;">
+                <a href="../projects/pre-Sportus.html">pre-Sport us!</a>
+              </li>
+
+             </ul>
+
+          </li>
         </ul>
       </nav>
     </header>
--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 /*
-    The showContent function accepts the name of a class to change and the class name to change it to as params.
-        It does so by looping througoh all elements, referencing their classnames and then changing matches.
+    The showOrHide function accepts the class of divs to show or hide, changing the 
+    classes of dropdown elements when necessary to correct border radius.
 */
 
-function showContent(matchClass, content) {
-    var elems = document.getElementsByTagName('*'),
-      i;
-    for (i in elems) {
-      if ((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ') >
-        -1) {
-        elems[i].className = content;
+function showOrHide(matchClass, bottomElementID) {
+
+  var elems = document.getElementsByTagName('*'),
+    i;
+  for (i in elems) {
+    if ((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ') >
+      -1) {
+      if (elems[i].style.display === "none") {
+        elems[i].style.display = "block";
+        document.getElementById(bottomElementID).classList.remove("bottom-dropdown-item");
+      } else {
+        elems[i].style.display = "none";
+        document.getElementById(bottomElementID).classList.add("bottom-dropdown-item");
       }
     }
   }
+}
