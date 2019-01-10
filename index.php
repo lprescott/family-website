7a50e2adfafcf0614e5ab357c6f5c58679bdc7d1
Luke Prescott <lukep@nycap.rr.com>
2019-01-10 17:38:15 -0500

    Added more responsive design for small displays

--- index.php
+++ index.php
@@ -39,7 +39,6 @@
 
 <body>
   <!--[if lte IE 9]>
-  onmouseout="changeCaret('desktop-menu-1', 0)"
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
   <![endif]-->
 
