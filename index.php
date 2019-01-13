97348a18226d9407bf5eef6fc3a0d51b53554198
Luke Prescott <lukep@nycap.rr.com>
2019-01-13 17:56:56 -0500

    Added php to contact form

--- index.php
+++ index.php
@@ -1,5 +1,86 @@
 <?php //begin php
-
+session_start();
+/*
+L.R.Prescott
+A PHP script to collect a client side form submission and email it to info@presport.us.
+6/9/2018
+*/
+if ($_POST['formSubmit'] == "Send Message") {
+	$message = "";
+	$title = "";
+	$icon = "";
+	// if google recaptcha is empty or name, email or comment werent supplied set error message
+	if (!isset($_POST['g-recaptcha-response']) || empty($_POST['g-recaptcha-response'])) {
+		$message = "Empty Google recaptcha.";
+		$title = "Error!";
+		$icon = "error";
+	}
+	else
+	if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['comment'])) {
+		$message = "Required information missing.";
+		$title = "Error!";
+		$icon = "error";
+	}
+	// check google recaptcha, if bot
+	$recaptchaResponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LcTgV0UAAAAADTU_7kiaFHUA6GI2Aq4JrsR6bLa&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
+	if ($recaptchaResponse . success == false) {
+		$message = "Recaptcha failed to authenticate.";
+		$title = "Error!";
+		$icon = "error";
+	}
+	// if error message isnt empty, exit
+	if (!empty($message)) {
+		$_SESSION['sessionMessage'] = $message;
+		$_SESSION['sessionTitle'] = $title;
+		$_SESSION['sessionIcon'] = $icon;
+		header("Location: submit.php");
+		exit;
+	}
+	$varSubject = "";
+	// store supplied info
+	$varName = $_POST['name'];
+	$varEmail = $_POST['email'];
+	$varSubject = $_POST['subject'];
+	$varComment = $_POST['comment'];
+	// if subject message isnt empty, set
+	if (empty($varSubject)) {
+		$varSubject = "PresPort Contact Form";
+	}
+	// dest
+	$to = "info@presport.us";
+	// today
+	$varToday = date("F j, Y, g:i a");
+	/* start message with header */
+	$varMessage = "<i>Automatically generated header</i><br />" . "From: " . $varName . " (" . $varEmail . ")<br />" . "Sent: " . $varToday . "<br />" . "To: " . $to . " (PresPort)<br />" . "Subject: " . $varSubject . "<br /><hr>";
+	// create headers for mail func
+	$headers = "From: " . $varEmail . "\r\n";
+	$headers.= "Reply-To: " . $varEmail . "\r\n";
+	$headers.= "MIME-Version: 1.0\r\n";
+	$headers.= "Content-Type: text/html; charset=UTF-8\r\n";
+	$varMessage.= $varComment;
+	if (mail($to, $varSubject, $varMessage, $headers)) {
+		// success here
+		$title = "Success!";
+		$message = "Your message has sent successfully.";
+		$icon = "success";
+		$_SESSION['sessionMessage'] = $message;
+		$_SESSION['sessionTitle'] = $title;
+    $_SESSION['sessionIcon'] = $icon;
+    header("Location: submit.php");
+		exit;
+	}
+	else {
+		// error
+		$message = "Could not send email.";
+		$title = "Error!";
+		$icon = "error";
+		$_SESSION['sessionMessage'] = $message;
+		$_SESSION['sessionTitle'] = $title;
+    $_SESSION['sessionIcon'] = $icon;
+    header("Location: submit.php");
+		exit;
+	}
+}
 ?>
 <!-- end php -->
 
@@ -227,10 +308,10 @@
         </script>
       </div>
       <div class="flex-row container">
-        <div class="item nobackground form-recaptcha" style="flex-basis: 50%; text-align: center;">
-          <div class="g-recaptcha" style="display: inline-block;" data-sitekey="6LcTgV0UAAAAAChaBvDs9rlYtpXPUeGJRF3Xzsrs"></div>
+        <div id="recaptcha-container" class="item nobackground form-recaptcha" style="flex-basis: 50%; text-align: right;">
+          <div style="display: inline-block;" class="g-recaptcha" data-sitekey="6LcTgV0UAAAAAChaBvDs9rlYtpXPUeGJRF3Xzsrs"></div>
         </div>
-        <div class="item nobackground form-submit" style="flex-basis: 50%; text-align: center;">
+        <div id="submit-container"  class="item nobackground form-submit" style="flex-basis: 50%; text-align: left;">
           <input style="display: inline-block;" type="submit" id="submit" name="formSubmit" value="Send Message" >
         </div>
       </div>
---
+++ submit.php
@@ -0,0 +1,64 @@
+<?php 
+session_start(); 
+require 'contact.php';
+$submitTitle = $_SESSION['sessionTitle'];
+$submitMessage = $_SESSION['sessionMessage'];
+$submitIcon = $_SESSION['sessionIcon'];
+?>
+
+<!doctype html>
+<html class="no-js" lang="eng">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>PresPort</title>
+  <meta name="description" content="Prescott family website.">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="apple-touch-icon" sizes="180x180" href="img/icons/apple-touch-icon.png">
+  <link rel="icon" type="image/x-icon" sizes="32x32" href="img/icons/favicon-32x32.png">
+  <link rel="icon" type="image/x-icon" sizes="16x16" href="img/icons/favicon-16x16.png">
+  <link rel="manifest" href="img/icons/site.webmanifest">
+  <link rel="mask-icon" href="img/icons/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="shortcut icon" href="img/icons/favicon.ico" type="image/x-icon">
+  <meta name="msapplication-TileColor" content="#2b5797">
+  <meta name="msapplication-config" content="img/icons/browserconfig.xml">
+  <meta name="theme-color" content="#ffffff">
+
+  <link rel="manifest" href="site.webmanifest">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <script src='https://www.google.com/recaptcha/api.js'></script>
+
+</head>
+
+<body>
+  <!-- scripts -->
+  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script>
+    window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')
+  </script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Sweetalert -->
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <script>
+    window.onload = function () {
+      swal({
+        title: "<?php echo $submitTitle ?>",
+        text: "<?php echo $submitMessage ?>",
+        icon: "<?php echo $submitIcon ?>",
+      });
+    }
+  </script>
+
+</body>
+
+</html>
