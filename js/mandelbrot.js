56456075237c6938eb5969bad1fccb16d5ffed2a
Luke Prescott <luke.prescott@presport.us>
2019-05-23 01:42:00 -0400

    Added controls to mandelbrot render

--- js/mandelbrot.js
+++ js/mandelbrot.js
@@ -3,14 +3,25 @@
   https://github.com/lprescott/
 */
 
+// Initialized global variables
 var scalar = 5;
 var xTranslate = 5;
-var yTranslate;
+var yTranslate; 
+var controlsWidth = 170; // The width of the left bar.
+var running = true;
+var fps = 10;           // The speed of the animation in frames/second.
 
+
+// Declared global variables
 var height;
 var width;
+var speedSlider;
+var xoffset;
+var yoffset;
 
-var controlsWidth = 170; // The width of the left bar.
+var redshift;
+var greenshift;
+var blueshift;
 
 function setup() {
 
@@ -24,17 +35,52 @@ function setup() {
   canvas.position(controlsWidth, 0);
   canvas.style('z-index', '-1');
 
-  // Number of iterations
-  createDiv('&nbsp;# of iterations');
-  iterations = createSlider(2,1000,50,10);
+  // Label for fps slider
+  createDiv('&nbsp;Fast Zoom ------- Stop');
+  // Slider for fps
+  speedSlider = createSlider(0.5, 1, 0.99, 0.001);
 
-  yTranslate = height / 2 - width / 2;
+  // Number of iterations
+  createDiv('<br>&nbsp;10 iterations ---- 1000');
+  iterations = createSlider(0,1000,20,10);
+
+  // x offset
+  createDiv('<br><br>&nbsp;X-Offset:');
+  xoffset = createSlider(-1,1,-0.805,0.01);
+
+  // y offset
+  createDiv('<br>&nbsp;Y-Offset:');
+  yoffset = createSlider(-250,250,0,1);
+
+  // red
+  createDiv('<br><br>&nbsp;Red:');
+  redshift = createSlider(-1,1,1,.1);
+
+  // green
+  createDiv('<br>&nbsp;Green:');
+  greenshift = createSlider(-1,1,1,.1);
+
+  // blue
+  createDiv('<br>&nbsp;Blue:');
+  blueshift = createSlider(-1,1,1,.1);
+
+  // Controls
+  createDiv('<br><br>&nbsp;Controls:'); 
+  createDiv('&nbsp;- SPACE: stop');
+
+  yTranslate = (height / 2 - width / 2) - yoffset.value();
+  //(1 / scalar) * (width * xoffset.value());
 }
 
 function draw() {
 
+  // This allows for a pause feature, see function keyPressed()
+  if(!running) return;
+
   //The maximum number of iterations to the below loop
   var maxIterations = iterations.value();
+
+  yTranslate = (height / 2 - width / 2) + yoffset.value();
 
   loadPixels();
   //This nested for-loop touches every pixel
@@ -78,14 +124,29 @@ function draw() {
       var red = map(sin(n * 0.25), -1, 1, 0, 145);
       var green = map(cos(n * 0.25), -1, 1, 170, 0);
       var blue = map(sin(n * 0.25), -1, 1, 150, 0);
+
       var pix = (x + y * width) * 4;
-      pixels[pix + 0] = red;
-      pixels[pix + 1] = green;
-      pixels[pix + 2] = blue;
+      pixels[pix + 0] = red * redshift.value();
+      pixels[pix + 1] = green * greenshift.value();
+      pixels[pix + 2] = blue * blueshift.value();
       pixels[pix + 3] = 255;
     }
   }
   updatePixels();
-  scalar *= 0.9;
-  xTranslate = (1 / scalar) * (width * 0.805);
+  scalar *= speedSlider.value();
+  xTranslate = (1 / scalar) * (width * -xoffset.value());
+}
+
+// The keyPressed() function is called once every time a key is 
+// pressed. 
+function keyPressed() {
+
+  // Play/pause on space
+  if (keyCode == 32 ) {
+      running = false;
+  } 
+}
+
+function setColorShift() {
+
 }
